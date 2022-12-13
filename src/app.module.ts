0edcaa16bb4b7f386e1965abffb7068088f962d1
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-13 19:46:07 -0300

    files: organizing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
+import { AppController } from './infra/app.controller';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
   imports: [],
---
+++ src/infra/http.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+})
+
+export class HttpModule {}
