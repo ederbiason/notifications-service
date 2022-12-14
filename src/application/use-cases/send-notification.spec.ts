470c108058db60c7b7768fd241dc5711a44d437d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-14 16:02:24 -0300

    in-memory db

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,32 +1,20 @@
 /* eslint-disable prettier/prettier */
 
-import { Notification } from "../entities/notification"
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository"
 import { SendNotification } from "./send-notification"
-
-// "Fake" database
-
-const notifications: Notification[] = [];
-
-// Persistence layer
-const notificationsRepository = {
-    async create(notification: Notification) {
-        notifications.push(notification)
-    }
-}
-
-// End of the "Fake" database
 
 describe('Send notification', () => {
     it('should be able to send a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository() 
         const sendNotification = new SendNotification(notificationsRepository)
 
-        await sendNotification.execute({
+        const { notification } = await sendNotification.execute({
             content: 'This is a notification',
-            category: 'social', 
+            category: 'social',   
             recipientId: 'exameple-recipient-id'
         })
 
-
-        expect(notifications).toHaveLength(1)
+        expect(notificationsRepository.notifications).toHaveLength(1)
+        expect(notificationsRepository.notifications[0]).toEqual(notification)
     })
 })
