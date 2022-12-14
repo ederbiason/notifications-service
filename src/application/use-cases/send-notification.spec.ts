47d275ab04c34cb1ab6520905d7ac6887f362e93
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-14 15:28:50 -0300

    test: Send notification

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { SendNotification } from "./send-notification"
+
+describe('Send notification', () => {
+    it('should be able to send a notification', async () => {
+        const sendNotification = new SendNotification()
+
+        const { notification } = await sendNotification.execute({
+            content: 'This is a notification',
+            category: 'social', 
+            recipientId: 'exameple-recipient-id'
+        })
+
+        expect(notification).toBeTruthy()
+    })
+})
