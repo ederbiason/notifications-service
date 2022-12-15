03146e4f33c00654ce4ee35a21377182ed7fe55e
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-15 17:22:49 -0300

    chore: Factory Pattern

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 
-import { Content } from "@application/entities/content";
-import { Notification } from "@application/entities/notification";
+import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "./cancel-notification";
 import { NotificationNotFound } from "./errors/notification-not-found";
@@ -11,11 +10,7 @@ describe('Cancel notification', () => {
         const notificationsRepository = new InMemoryNotificationsRepository() 
         const cancelNotification = new CancelNotification(notificationsRepository)
 
-        const notification = new Notification({
-            category: 'social',
-            content: new Content('Novo solicitação de amizade!'),
-            recipientId: 'example-recipient-id',
-        });
+        const notification = makeNotification();
 
         await notificationsRepository.create(notification);
 
