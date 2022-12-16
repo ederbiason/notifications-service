342b0da7c675a65049eeda60643d0ff29b233a07
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-16 15:37:01 -0300

    chore: Handle Send Notification

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,12 +1,15 @@
 /* eslint-disable prettier/prettier */
 
+import { SendNotification } from "@application/use-cases/send-notification";
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "../database/database.module";
+import { NotificationsController } from "./kafka/controllers/notifications.controller";
 import { KafkaConsumerService } from "./kafka/kafka-consumer.service";
 
 @Module({
-    imports: [],
-    providers: [KafkaConsumerService],
-    controllers: []
+    imports: [DatabaseModule],
+    providers: [KafkaConsumerService, SendNotification],
+    controllers: [NotificationsController]
 })
 
 export class MessagingModule {
